7c5e2850c0c865a7b398623837c5621a6ef9033a
Edgard Lima <edgard.lima@accenture.com>
2018-05-02 14:01:54 -0300

    fix build

--- edgard_tools/build.sh
+++ edgard_tools/build.sh
@@ -6,16 +6,16 @@ IMAGE=edgard_tools
 
 # download cudnn
 CUDNN_URL="http://developer.download.nvidia.com/compute/redist/cudnn/v7.1.3/cudnn-9.0-linux-x64-v7.1.tgz"
-( curl -Lo res/cudnn-9.0-linux-x64-v7.1.tgz -C - $CUDNN_URL ) || echo "failed to download cudnn" ; exit
+( wget --max-redirect=9 -c $CUDNN_URL -O res/cudnn-9.0-linux-x64-v7.1.tgz ) || { echo "failed to download cudnn" ; exit; }
 
 # download qt installer
 QT_URL="https://download.qt.io/official_releases/qt/5.10/5.10.1/qt-opensource-linux-x64-5.10.1.run"
-( curl -Lo res/qt_installer.run -C - $QT_URL ) || echo "failed to download qt" ; exit
+( wget --max-redirect=9 -c $QT_URL -O res/qt_installer.run ) || { echo "failed to download qt-installer" ; exit; }
 
 # download nvidia driver
 nvidia_version=$(cat /proc/driver/nvidia/version | head -n 1 | awk '{ print $8 }')
 nvidia_driver_uri=http://us.download.nvidia.com/XFree86/Linux-x86_64/${nvidia_version}/NVIDIA-Linux-x86_64-${nvidia_version}.run
-( curl -Lo res/nvidia-driver.run -C - $nvidia_driver_uri ) || echo "failed to download driver" ; exit
+( wget --max-redirect=9 -c $nvidia_driver_uri -O res/nvidia-driver.run ) || { echo "failed to nvidia-driver" ; exit; }
 
 YOLO_WEIGHTS="y"
 
@@ -34,10 +34,10 @@ done
 
 # download yolo weights
 if [ "$YOLO_WEIGHTS" == "y" ]; then
-    ( curl -Lo res/yolov2.weights -C - https://pjreddie.com/media/files/yolov2.weights ) || echo "failed to download yolo2" ; exit
-    ( curl -Lo res/yolov2-voc.weights -C - https://pjreddie.com/media/files/yolov2-voc.weights ) || echo "failed to download yolo2voc" ; exit
-    ( curl -Lo res/yolov2-tiny.weights -C - https://pjreddie.com/media/files/yolov2-tiny.weights ) || echo "failed to download yolo2tiny" ; exit
-    ( curl -Lo res/yolov2-tiny-voc.weights -C - https://pjreddie.com/media/files/yolov2-tiny-voc.weights ) || echo "failed to download yolo2tiny-voc" ; exit
+	( wget --max-redirect=9 -c https://pjreddie.com/media/files/yolov2.weights -O res/yolov2.weights ) || { echo "failed to download yolov2.weights" ; exit; }
+	( wget --max-redirect=9 -c https://pjreddie.com/media/files/yolov2-voc.weights -O res/yolov2-voc.weights ) || { echo "failed to download yolov2-voc.weights" ; exit; }
+	( wget --max-redirect=9 -c https://pjreddie.com/media/files/yolov2-tiny.weights -O res/yolov2-tiny.weights ) || { echo "failed to download yolov2-tiny.weights" ; exit; }
+	( wget --max-redirect=9 -c https://pjreddie.com/media/files/yolov2-tiny-voc.weights -O res/yolov2-tiny-voc.weights ) || { echo "failed to download yolov2-tiny-voc.weight" ; exit; }
 fi
 
 docker build -t $IMAGE .
--- edgard_tools_test_env/build.sh
+++ edgard_tools_test_env/build.sh
@@ -7,7 +7,7 @@ IMAGE=edgard_tools_env
 # download nvidia driver
 nvidia_version=$(cat /proc/driver/nvidia/version | head -n 1 | awk '{ print $8 }')
 nvidia_driver_uri=http://us.download.nvidia.com/XFree86/Linux-x86_64/${nvidia_version}/NVIDIA-Linux-x86_64-${nvidia_version}.run
-( curl -Lo res/nvidia-driver.run -C - $nvidia_driver_uri ) || echo "failed to download nvidia driver" ; exit
+( wget --max-redirect=9 -c $nvidia_driver_uri -O res/nvidia-driver.run ) || { echo "failed to nvidia-driver" ; exit; }
 
 docker build -t $IMAGE .
 
